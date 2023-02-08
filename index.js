4bb555075e9dd75b794c56c72753d39aaac2beb0
AntonDzyubenkoGit <107909682+AntonDzyubenkoGit@users.noreply.github.com>
2023-02-08 09:50:19 +0300

    Функция is()

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
-import _ from "lodash";
-// Функция normalize(), которая "нормализует" данные переданного урока
+// Функция is(), которая сравнивает объекты по совпадению данных, а не по ссылкам
 
-const normalize = (obj) => {
-  obj.name = _.capitalize(obj.name);
-  obj.description = obj.description.toLowerCase();
-  return obj;
+const is = (company1, company2) => {
+  const keys = ["name", "state", "website"];
+
+  for (const key of keys) {
+    if (company1[key] !== company2[key]) {
+      return false;
+    }
+  }
+  return true;
 };
 
-const lesson = {
-  name: "ДеструКТУРИЗАЦИЯ",
-  description: "каК удивитЬ друзей",
+const company1 = {
+  name: "Hexlet",
+  state: "published",
+  website: "https://hexlet.io",
+};
+const company2 = {
+  name: "Hexlet",
+  state: "published",
+  website: "https://hexlet.io",
 };
 
-console.log(normalize(lesson));
+console.log(is(company1, company2));
