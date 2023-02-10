5b0350d58183515d05ee9a4f6b4384f84bab21f8
AntonDzyubenkoGit <107909682+AntonDzyubenkoGit@users.noreply.github.com>
2023-02-10 11:32:21 +0300

    Функция cloneShallow ()

--- index.js
+++ index.js
@@ -1,37 +1,25 @@
 import path from "node:path";
 import _ from "lodash";
 
-// Функция fill(), которая заполняет объект данными из другого объекта по разрешенному списку ключей.
-
-const company = {
-  name: "testdrive",
-  state: "moderating",
-  age: 48,
-};
+// Функция cloneShallow (), которая выполняет поверхностное копирование объектов.
 
 const data = {
-  name: "Hexlet",
-  state: "published",
-  age: 12,
+  key: "value",
+  key2: {
+    key: "innerValue",
+    innerKey: {
+      anotherKey: "anotherValue",
+    },
+  },
 };
 
-const fill = (object, keys, data) => {
-  const lengthOfProperties = keys.length;
-
-  if (lengthOfProperties === 0) {
-    return Object.assign(object, keys);
+const cloneShallow = (obj) => {
+  const result = {};
+  const entries = Object.entries(obj);
+  for (const [key, value] of entries) {
+    result[key] = value;
   }
-
-  const pick = _.pick(data, keys);
-  const result = Object.assign(object, pick);
-
   return result;
 };
 
-// Версия в две строки
-const fill2 = (object, keys, data) => {
-  const pickFilter = keys.length > 0 ? _.pick(data, keys) : data;
-  return Object.assign(object, pickFilter);
-};
-
-console.log(fill2(company, ["name", "state", "age"], data));
+console.log(cloneShallow(data));
